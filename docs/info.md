9f90e8327b381806d022f824dfe43fa04fc7e694
Mathieu Escouteloup <mathieu.escoueloup@ims-bordeaux.fr>
2025-06-27 07:13:43 +0200

    Initialize with generated SystemVerilog sources

--- docs/info.md
+++ docs/info.md
@@ -9,12 +9,12 @@ You can also include images in this folder and reference them in the markdown. E
 
 ## How it works
 
-Explain how your project works
+It is a simple RISC-V 32-bit implementation.
 
 ## How to test
 
-Explain how to use your project
+There is no implemented test yet.
 
 ## External hardware
 
-List external hardware used in your project (e.g. PMOD, LED display, etc), if any
+1 input to select boot loader, 1 input for UART Rx, 1 output for Uart Tx, 8 input/outputs for GPIOs
--- src/Top.v
+++ src/Top.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module tt_um_example (
+module tt_um_emmk_riscv (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
@@ -15,13 +15,21 @@ module tt_um_example (
     input  wire       clk,      // clock
     input  wire       rst_n     // reset_n - low to reset
 );
+    
+  // Default
+  assign uo_out[7:1]  = ui_in[7:2] + uio_in[7:2];
+  wire _unused = &{ena, 7'b000000};
 
-  // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
-
-  // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
-
+  Sys m_riscV(
+    .clock          (clk        ),
+    .reset          (!rst_n     ),
+     
+    .io_i_boot      (ui_in[1]   ),
+    .io_b_uart_rx   (ui_in[0]   ),
+    .io_b_uart_tx   (uo_out[0]  ),
+     
+    .io_b_gpio_eno  (uio_oe     ),
+    .io_b_gpio_out  (uio_out    ),
+    .io_b_gpio_in   (uio_in     )
+  );
 endmodule
